b5e12bf309a09be96ddc06951efc99c474d09aa2
David-Mateus <david-mateus09@hotmail.com>
2023-10-05 12:42:00 -0300

    Create userController

---
+++ src/main/java/com/example/movies/controllers/UserController.java
@@ -0,0 +1,49 @@
+package com.example.movies.controllers;
+
+import com.example.movies.models.UserModel;
+import com.example.movies.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+
+@RestController
+@RequestMapping("/user")
+@Validated
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserModel> findById(@PathVariable Long id){
+        UserModel obj = this.userService.findyById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+    @PostMapping
+    @Validated
+    public ResponseEntity<Void> create(@Valid @RequestBody UserModel obj){
+        this.userService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+    @PutMapping("/{id}")
+    @Validated
+    public ResponseEntity<Void> update(@Valid @RequestBody UserModel obj, @PathVariable Long id){
+        obj.setId(id);
+        this.userService.update(obj);
+        return  ResponseEntity.noContent().build();
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        this.userService.delete(id);
+        return ResponseEntity.noContent().build();
+
+    }
+}
